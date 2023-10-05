39787f88bbee0427a293b0ad4dd535219d215051
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-10-05 08:13:18 -0300

    control de flujo parte tres

---
+++ ejercicios/02-control/09-switch.dart
@@ -0,0 +1,21 @@
+import 'dart:math';
+
+main() {
+  int rnd = Random().nextInt(7);
+
+  // if ( rnd == 0 ) {
+  //   print('Lunes');
+  // } else if ( rnd == 1 ) {
+  //   print('Martes');
+  // } else if ( rnd == 2 ) {
+  //   print('Miércoles');
+  // } else if ( rnd == 3 ) {
+  //   print('Jueves');
+  // } else {
+  //   print('No es un día de la semana');
+  // }
+  
+  rnd = 10;
+
+  print(rnd);
+}
