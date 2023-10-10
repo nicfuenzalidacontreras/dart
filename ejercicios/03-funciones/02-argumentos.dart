a9d493e5a63be02fd7a787292009ef104d165f2f
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-10-10 08:39:29 -0300

    funciones parte dos

--- ejercicios/03-funciones/02-argumentos.dart
+++ ejercicios/03-funciones/02-argumentos.dart
@@ -7,7 +7,7 @@ void saludar2( String mensaje, { required String nombre, int veces = 10 }) {
   print('Saludar2: $mensaje $nombre - $veces');
 }
 
-main(List<String> args) {
+main(List<String> args) { 
   saludar('Hola', 'Fernando', 35);
 
   saludar2( 'Saludos', veces: 20, nombre: 'Tony' );
---
+++ ejercicios/03-funciones/03-valor-referencia.dart
@@ -0,0 +1,30 @@
+String capitalizar( String texto ) {
+  texto = texto.toUpperCase();
+  return texto;
+}
+
+Map<String, String> capitalizarMapa( Map<String, String> mapa ) {
+  // Romprer la referencia
+  mapa = { ...mapa };
+  mapa['nombre'] = mapa['nombre']?.toUpperCase() ?? 'No hay nombre';
+
+  return mapa;
+}
+
+
+
+main(List<String> args) {
+  String nombre  = 'nicolás';
+  String nombre2 = capitalizar(nombre);
+
+  // print( nombre );
+  // print( nombre2 );
+
+  Map<String, String> persona = {
+    'nombre': 'Tony Stark'
+  };
+  Map<String, String> persona2 = capitalizarMapa( persona );
+
+  print( persona );
+  print( persona2 );
+}
