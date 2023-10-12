7a03956e808c61e67ea416d002a9225a58281f45
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-10-12 08:22:53 -0300

    tipos no comunes parte dos

---
+++ ejercicios/04-tipos-nocomunes/04-future2.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+main() {
+  // OSX y Linux
+  File file = new File( Directory.current.path + '/04-tipos-nocomunes/assets/personas.txt' );
+
+  // Windows
+  // File file = new File( Directory.current.path + '\\04_tipos_nocomunes\\assets\\personas.txt' );
+
+  Future<String> f = file.readAsString();
+  // String f = file.readAsStringSync();
+
+  f.then( (data) => print(data) );
+  // f.then( print );
+
+  // print(f);
+  print( 'Fin del main' );
+}
