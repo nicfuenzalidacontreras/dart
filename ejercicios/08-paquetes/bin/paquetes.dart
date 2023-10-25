9dffede061507544703a3b7a2762000db3ff1465
Nicolás Fuenzalida <na.fuenzalida.contreras@gmail.com>
2023-10-25 08:31:38 -0300

    paquetes, ejecutar programa y http parte tres

--- ejercicios/08-paquetes/bin/paquetes.dart
+++ ejercicios/08-paquetes/bin/paquetes.dart
@@ -1,13 +1,9 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 import 'package:paquetes/paquetes.dart' as paquetes;
 
 void main(List<String> arguments) {
-  final url = Uri.parse('https://reqres.in/api/users?page=2');
-  http.get(url).then((res) {
-    //print(res);
-    final body = jsonDecode(res.body);
-    print(body);
-    print('page: ${body['paga']}');
-  });
+  
+  // paquetes.getReqResp_service();
+
+  paquetes.getPais();
+
 }
